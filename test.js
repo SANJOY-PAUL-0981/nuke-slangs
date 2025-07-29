3c7953bd853509749467909f67ad83c5446a6e6b
SANJOY <paulsanjoy2923@gmail.com>
2025-07-29 19:41:32 +0530

    updated

--- test.js
+++
@@ -1,7 +0,0 @@
-const { isSlang } = require('./src/index');
-const { makeClean } = require('./src/index')
-
-console.log(isSlang("you are an ass"));
-console.log(makeClean("you are classy")); 
-console.log(isSlang("class")); 
-console.log(makeClean("dumb bokachoda"));
